c3a48059e29aec0d558cea98103513ef13ef0708
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:44:07 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao17/main.dart
+++ estudo-dirigido-1.1/lib/questao17/main.dart
@@ -1,4 +1,19 @@
-
 void main() {
-  print("Questão 17 implementada.");
+  var s = Semaforo();
+  s.mudar();
+  print(s.cor);
+}
+
+class Semaforo {
+  String cor = "Vermelho";
+
+  void mudar() {
+    if (cor == "Vermelho") {
+      cor = "Verde";
+    } else if (cor == "Verde") {
+      cor = "Amarelo";
+    } else {
+      cor = "Vermelho";
+    }
+  }
 }
