2988e2cb560d5402a801f269cea5dfcee3a2c43d
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:45:54 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao19/main.dart
+++ estudo-dirigido-1.1/lib/questao19/main.dart
@@ -1,4 +1,25 @@
-
 void main() {
-  print("Questão 19 implementada.");
+  var c = Casa();
+  c.add(Comodo(10));
+  print(c.total());
+}
+
+class Comodo {
+  double area;
+
+  Comodo(this.area);
+}
+
+class Casa {
+  List<Comodo> lista = [];
+
+  void add(Comodo c) => lista.add(c);
+
+  double total() {
+    double t = 0;
+    for (var c in lista) {
+      t += c.area;
+    }
+    return t;
+  }
 }
