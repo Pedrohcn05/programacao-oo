8b9c1a6db3e52dccab59db084ef61d45ad97b081
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 14:53:55 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao06/main.dart
+++ estudo-dirigido-1.1/lib/questao06/main.dart
@@ -1,17 +1,12 @@
-void main() {
-  var p = Produto("Caneta", 2, 15);
-  print(p.total());
-}
+public class Main {
+    public static void main(String[] args) {
+        Produto produto = new Produto(1, "Notebook", 3500.00, 3);
+        produto.exibirResumo();
 
-class Produto {
-  String nome;
-  double preco;
-  int qtd;
-
-  Produto(this.nome, this.preco, this.qtd);
-
-  double total() {
-    double desconto = qtd > 10 ? 0.1 : 0;
-    return preco * qtd * (1 - desconto);
-  }
+        produto.setCodigo(2);
+        produto.setNome("Smartphone");
+        produto.setPrecoUnitario(1500.00);
+        produto.setQuantidade(12);
+        produto.exibirResumo();
+    }
 }
