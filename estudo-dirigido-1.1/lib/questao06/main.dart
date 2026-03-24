80fead0e32f4257c598b5e3f84d70861edfda1f9
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:35:16 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao06/main.dart
+++ estudo-dirigido-1.1/lib/questao06/main.dart
@@ -1,4 +1,17 @@
-
 void main() {
-  print("Questão 6 implementada.");
+  var p = Produto("Caneta", 2, 15);
+  print(p.total());
+}
+
+class Produto {
+  String nome;
+  double preco;
+  int qtd;
+
+  Produto(this.nome, this.preco, this.qtd);
+
+  double total() {
+    double desconto = qtd > 10 ? 0.1 : 0;
+    return preco * qtd * (1 - desconto);
+  }
 }
