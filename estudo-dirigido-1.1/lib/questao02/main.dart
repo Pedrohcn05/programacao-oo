ee77292cbd4e5cb3ee238a3b0dbf36b4a0d70422
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:29:28 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao02/main.dart
+++ estudo-dirigido-1.1/lib/questao02/main.dart
@@ -1,4 +1,26 @@
-
 void main() {
-  print("Questão 2 implementada.");
+  var r = Relogio(23, 59, 59);
+  r.incrementar();
+  print(r);
+}
+
+class Relogio {
+  int h, m, s;
+
+  Relogio(this.h, this.m, this.s);
+
+  void incrementar() {
+    s++;
+    if (s == 60) {
+      s = 0;
+      m++;
+    }
+    if (m == 60) {
+      m = 0;
+      h++;
+    }
+    if (h == 24) h = 0;
+  }
+
+  String toString() => "$h:$m:$s";
 }
