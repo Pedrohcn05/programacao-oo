88defa350668b7850d1a667a60b6ab1e1f71d677
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:37:27 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao09/main.dart
+++ estudo-dirigido-1.1/lib/questao09/main.dart
@@ -1,4 +1,21 @@
-
 void main() {
-  print("Questão 9 implementada.");
+  var e = Endereco("Rua B", "SP");
+  print(Pessoa("Pedro", e));
+}
+
+class Endereco {
+  String rua, cidade;
+
+  Endereco(this.rua, this.cidade);
+
+  String toString() => "$rua - $cidade";
+}
+
+class Pessoa {
+  String nome;
+  Endereco endereco;
+
+  Pessoa(this.nome, this.endereco);
+
+  String toString() => "$nome | $endereco";
 }
