a3293f79e6535b2ba7d2585d9502562cd122f8b9
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:45:15 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao18/main.dart
+++ estudo-dirigido-1.1/lib/questao18/main.dart
@@ -1,4 +1,25 @@
-
 void main() {
-  print("Questão 18 implementada.");
+  var p = Playlist();
+  p.add(Musica(200));
+  print(p.total());
+}
+
+class Musica {
+  int duracao;
+
+  Musica(this.duracao);
+}
+
+class Playlist {
+  List<Musica> lista = [];
+
+  void add(Musica m) => lista.add(m);
+
+  int total() {
+    int t = 0;
+    for (var m in lista) {
+      t += m.duracao;
+    }
+    return t;
+  }
 }
