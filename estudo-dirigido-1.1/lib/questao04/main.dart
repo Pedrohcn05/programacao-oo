9d430d9556d4a5328e28bd14824d84bbe1769006
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 00:34:24 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao04/main.dart
+++ estudo-dirigido-1.1/lib/questao04/main.dart
@@ -1,4 +1,12 @@
-
 void main() {
-  print("Questão 4 implementada.");
+  var d = Data(10, 3, 2025);
+  print(d);
+}
+
+class Data {
+  int dia, mes, ano;
+
+  Data(this.dia, this.mes, this.ano);
+
+  String toString() => "$dia/$mes/$ano";
 }
