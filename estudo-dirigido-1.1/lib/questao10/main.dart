3fe345cb2bd033d7d496b337e649622352b5ed6b
Pedrohcn05 <phcn.2005@gmail.com>
2026-03-24 15:03:50 -0300

    Update main.dart

--- estudo-dirigido-1.1/lib/questao10/main.dart
+++ estudo-dirigido-1.1/lib/questao10/main.dart
@@ -1,12 +1,28 @@
-void main() {
-  var q = Quadrado(4);
-  print(q.area());
-}
+public class Main {
+    public static void main(String[] args) {
+        Quadrado q1 = new Quadrado(4, "*");
+        Quadrado q2 = new Quadrado(6, "#");
 
-class Quadrado {
-  double lado;
+        Quadrado q3 = new Quadrado(5, "@");
+        Quadrado q4 = new Quadrado(5, "@");
 
-  Quadrado(this.lado);
+        q1.exibirResumo();
+        q1.desenhar();
+        System.out.println();
 
-  double area() => lado * lado;
+        q2.exibirResumo();
+        q2.desenhar();
+        System.out.println();
+
+        q3.exibirResumo();
+        q3.desenhar();
+        System.out.println();
+
+        q4.exibirResumo();
+        q4.desenhar();
+        System.out.println();
+
+        System.out.println("q1 é igual a q2? " + q1.ehIgual(q2));
+        System.out.println("q3 é igual a q4? " + q3.ehIgual(q4));
+    }
 }
