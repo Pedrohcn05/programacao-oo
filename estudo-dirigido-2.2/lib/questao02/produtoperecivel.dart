5774834f80284126eed68b6173ed4d3977cc408d
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 21:23:21 -0300

    Create produtoperecivel.dart

---
+++ estudo-dirigido-2.2/lib/questao02/produtoperecivel.dart
@@ -0,0 +1,15 @@
+class ProdutoPerecivel extends Produto implements Exibivel {
+ String dataValidade;
+ ProdutoPerecivel(
+ String nome,
+ double preco,
+ this.dataValidade,
+ ) : super(nome, preco);
+ @override
+ void exibir() {
+ print('Produto perecível');
+ print('Nome: $nome');
+ print('Preço: R\$ $preco');
+ print('Data de validade: $dataValidade');
+ }
+}
