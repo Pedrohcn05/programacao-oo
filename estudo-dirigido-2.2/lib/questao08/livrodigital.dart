2f75953c26f700e0eedb1a23df336cb0c5a5a68e
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 22:06:02 -0300

    Create livrodigital.dart

---
+++ estudo-dirigido-2.2/lib/questao08/livrodigital.dart
@@ -0,0 +1,15 @@
+class LivroDigital extends Livro implements Informativo {
+  double tamanhoArquivo;
+
+  LivroDigital(String titulo, String autor, this.tamanhoArquivo)
+      : super(titulo, autor);
+
+  @override
+  void exibirInformacoes() {
+    print('Livro Digital');
+    print('Título: $titulo');
+    print('Autor: $autor');
+    print('Tamanho do arquivo: ${tamanhoArquivo}MB');
+    print('-------------------------');
+  }
+}
