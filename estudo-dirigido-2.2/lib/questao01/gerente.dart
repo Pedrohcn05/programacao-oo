16df7292c665f98a1534696ae302b991f5ee798c
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 21:20:36 -0300

    Create gerente.dart

---
+++ estudo-dirigido-2.2/lib/questao01/gerente.dart
@@ -0,0 +1,12 @@
+class Gerente extends Funcionario {
+ String setor;
+ Gerente(String nome, double salario, this.setor) : super(nome, salario);
+ @override
+ void exibirDados() {
+ print('Gerente');
+ print('Nome: $nome');
+ print('Salário: R\$ $salario');
+ print('Setor: $setor');
+ print('');
+ }
+}
