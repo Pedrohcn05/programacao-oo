fd4aa121c883584c95120fe5d1c0f371e08d80da
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 21:30:47 -0300

    Create moto.dart

---
+++ estudo-dirigido-2.2/lib/questao03/moto.dart
@@ -0,0 +1,19 @@
+class Moto extends Veiculo {
+  int cilindradas;
+
+  Moto(
+    String marca,
+    String modelo,
+    int ano,
+    this.cilindradas,
+  ) : super(marca, modelo, ano);
+
+  @override
+  void exibirFicha() {
+    print('--- Ficha da Moto ---');
+    print('Marca: $marca');
+    print('Modelo: $modelo');
+    print('Ano: $ano');
+    print('Cilindradas: $cilindradas cc');
+  }
+}
