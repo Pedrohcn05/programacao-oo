268b429305d6a9204f2e8b81d0a1c53a2d877ca2
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 22:19:55 -0300

    Create inimigo.dart

---
+++ estudo-dirigido-2.2/lib/questao11/inimigo.dart
@@ -0,0 +1,8 @@
+abstract class Inimigo extends Personagem {
+  int recompensa;
+
+  Inimigo(String nome, int vida, int nivel, this.recompensa)
+      : super(nome, vida, nivel);
+
+  void exibirRecompensa();
+}
