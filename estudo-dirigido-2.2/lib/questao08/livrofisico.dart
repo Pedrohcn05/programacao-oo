ccc4688b338b1e68534c7954ea0f5fec0a96e09e
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 22:07:34 -0300

    Create livrofisico.dart

---
+++ estudo-dirigido-2.2/lib/questao08/livrofisico.dart
@@ -0,0 +1,15 @@
+class LivroFisico extends Livro implements Informativo {
+  int quantidadePaginas;
+
+  LivroFisico(String titulo, String autor, this.quantidadePaginas)
+      : super(titulo, autor);
+
+  @override
+  void exibirInformacoes() {
+    print('Livro Físico');
+    print('Título: $titulo');
+    print('Autor: $autor');
+    print('Quantidade de páginas: $quantidadePaginas');
+    print('-------------------------');
+  }
+}
