a4e22b2b825dffc93ba62e7005b1f64519b3356f
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 21:19:33 -0300

    Create funcionariocomum.dart

---
+++ estudo-dirigido-2.2/lib/questao01/funcionariocomum.dart
@@ -0,0 +1,10 @@
+class FuncionarioComum extends Funcionario {
+ FuncionarioComum(String nome, double salario) : super(nome, salario);
+ @override
+ void exibirDados() {
+ print('Funcionário comum');
+ print('Nome: $nome');
+ print('Salário: R\$ $salario');
+ print('');
+ }
+}
