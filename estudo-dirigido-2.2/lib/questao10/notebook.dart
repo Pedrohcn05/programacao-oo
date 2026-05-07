edd057463a4ea2ad60d68a40092dbf8facd2f2d3
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 22:13:07 -0300

    Create notebook.dart

---
+++ estudo-dirigido-2.2/lib/questao10/notebook.dart
@@ -0,0 +1,19 @@
+class Notebook extends Dispositivo implements Ligavel {
+  int memoriaRam;
+
+  Notebook(
+    String marca,
+    String modelo,
+    this.memoriaRam,
+  ) : super(marca, modelo);
+
+  @override
+  void ligar() {
+    print('O notebook $marca $modelo está ligando...');
+  }
+
+  @override
+  void desligar() {
+    print('O notebook $marca $modelo está desligando...');
+  }
+}
