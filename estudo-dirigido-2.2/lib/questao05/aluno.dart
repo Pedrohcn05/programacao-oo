2977cb76f89f5e1ef65a9a3988c9b6b790df904c
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-06 21:37:44 -0300

    Create aluno.dart

---
+++ estudo-dirigido-2.2/lib/questao05/aluno.dart
@@ -0,0 +1,20 @@
+class Aluno extends Pessoa {
+  String matricula;
+  String curso;
+
+  Aluno(
+    String nome,
+    int idade,
+    this.matricula,
+    this.curso,
+  ) : super(nome, idade);
+
+  @override
+  void exibirDados() {
+    print('--- Dados do Aluno ---');
+    print('Nome: $nome');
+    print('Idade: $idade');
+    print('Matrícula: $matricula');
+    print('Curso: $curso');
+  }
+}
