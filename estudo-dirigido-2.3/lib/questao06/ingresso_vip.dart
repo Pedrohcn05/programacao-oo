4ac2c2cef3e1ab98c478e18dc92be0a89fde476c
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-13 22:04:20 -0300

    Create ingresso_vip.dart

---
+++ estudo-dirigido-2.3/lib/questao06/ingresso_vip.dart
@@ -0,0 +1,24 @@
+class IngressoVip extends Ingresso
+    implements Calculavel {
+
+  double valorAdicional;
+
+  IngressoVip(
+    String evento,
+    double valor,
+    this.valorAdicional,
+  ) : super(evento, valor);
+
+  @override
+  double valorFinal() {
+    return valor + valorAdicional;
+  }
+
+  @override
+  String toString() {
+    return 'Ingresso VIP | '
+        'Evento: $evento | '
+        'Valor original: R\$ ${valor.toStringAsFixed(2)} | '
+        'Valor final: R\$ ${valorFinal().toStringAsFixed(2)}';
+  }
+}
