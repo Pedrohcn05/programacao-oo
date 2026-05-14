28c00ca3a5868c7eb797c82e83f5bf3e2ae8bf30
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-13 21:57:31 -0300

    Create aluno.dart

---
+++ estudo-dirigido-2.3/lib/questao05/aluno.dart
@@ -0,0 +1,25 @@
+class Aluno extends Pessoa implements Apresentavel {
+  String matricula;
+  String curso;
+
+  Aluno(
+    String nome,
+    int idade,
+    this.matricula,
+    this.curso,
+  ) : super(nome, idade);
+
+  @override
+  void exibirDados() {
+    print(toString());
+  }
+
+  @override
+  String toString() {
+    return 'Aluno | '
+        'Nome: $nome | '
+        'Idade: $idade anos | '
+        'Matrícula: $matricula | '
+        'Curso: $curso';
+  }
+}
