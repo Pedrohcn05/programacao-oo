3af70d37ffc6b7a4c0d316fe8f799fb1537632bd
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-13 21:59:13 -0300

    Create coordenador.dart

---
+++ estudo-dirigido-2.3/lib/questao05/coordenador.dart
@@ -0,0 +1,87 @@
+class Coordenador extends Pessoa
+    implements Apresentavel {
+
+  String area;
+  int tempoExperiencia;
+
+  Coordenador(
+    String nome,
+    int idade,
+    this.area,
+    this.tempoExperiencia,
+  ) : super(nome, idade);
+
+  @override
+  void exibirDados() {
+    print(toString());
+  }
+
+  @override
+  String toString() {
+    return 'Coordenador | '
+        'Nome: $nome | '
+        'Idade: $idade anos | '
+        'Área: $area | '
+        'Experiência: $tempoExperiencia anos';
+  }
+}
+
+void main() {
+  // Criando os objetos
+  var ana = Aluno(
+    'Ana',
+    19,
+    'A001',
+    'ADS',
+  );
+
+  var bruno = Aluno(
+    'Bruno',
+    22,
+    'A002',
+    'Engenharia de Software',
+  );
+
+  var carla = Professor(
+    'Carla',
+    38,
+    'POO',
+    2000.00,
+  );
+
+  var diego = Professor(
+    'Diego',
+    41,
+    'Banco de Dados',
+    1900.00,
+  );
+
+  var elisa = Coordenador(
+    'Elisa',
+    45,
+    'Tecnologia',
+    15,
+  );
+
+  // Lista genérica usando a interface
+  ListaGenerica<Apresentavel> pessoas =
+      ListaGenerica<Apresentavel>();
+
+  // Adicionando objetos
+  pessoas.adicionar(ana);
+  pessoas.adicionar(bruno);
+  pessoas.adicionar(carla);
+  pessoas.adicionar(diego);
+  pessoas.adicionar(elisa);
+
+  // Imprimindo usando imprimirItens()
+  print('--- imprimirItens() ---');
+  pessoas.imprimirItens();
+
+  // Loop chamando exibirDados()
+  print('\n--- Método exibirDados() ---');
+
+  for (var pessoa in pessoas.obterItens()) {
+    pessoa.exibirDados();
+  }
+}
