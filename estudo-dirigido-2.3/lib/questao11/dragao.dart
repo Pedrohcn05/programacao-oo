2b891a1ffc2495a062a9d2ad76fd0509627e56cf
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-18 19:45:38 -0300

    Create dragao.dart

---
+++ estudo-dirigido-2.3/lib/questao11/dragao.dart
@@ -0,0 +1,23 @@
+class Dragao extends Inimigo implements Flamejante {
+  int poderFogo;
+
+  Dragao(
+    String nome,
+    int vida,
+    int nivel,
+    int recompensa,
+    this.poderFogo,
+  ) : super(nome, vida, nivel, recompensa);
+
+  @override
+  void soltarFogo() {
+    print('$nome soltou fogo devastador!');
+  }
+
+  @override
+  String toString() {
+    return 'Dragão -> Nome: $nome, Vida: $vida, '
+        'Nível: $nivel, Recompensa: $recompensa, '
+        'Poder de Fogo: $poderFogo';
+  }
+}
