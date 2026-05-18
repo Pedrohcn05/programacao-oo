38ef6b2ba0503df502d9ffa4fa6c39dbee12450b
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-18 19:45:04 -0300

    Create goblin.dart

---
+++ estudo-dirigido-2.3/lib/questao11/goblin.dart
@@ -0,0 +1,18 @@
+class Goblin extends Inimigo {
+  int velocidade;
+
+  Goblin(
+    String nome,
+    int vida,
+    int nivel,
+    int recompensa,
+    this.velocidade,
+  ) : super(nome, vida, nivel, recompensa);
+
+  @override
+  String toString() {
+    return 'Goblin -> Nome: $nome, Vida: $vida, '
+        'Nível: $nivel, Recompensa: $recompensa, '
+        'Velocidade: $velocidade';
+  }
+}
