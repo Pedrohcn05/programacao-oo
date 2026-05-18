34db9a6c1586c4026dfd41bb525501436b80265e
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-18 19:33:46 -0300

    Create smartphone.dart

---
+++ estudo-dirigido-2.3/lib/questao10/smartphone.dart
@@ -0,0 +1,26 @@
+class Smartphone extends Dispositivo implements Ligavel {
+  String sistemaOperacional;
+
+  Smartphone(
+    String marca,
+    String modelo,
+    this.sistemaOperacional,
+  ) : super(marca, modelo);
+
+  @override
+  void ligar() {
+    print('$marca $modelo está ligando...');
+  }
+
+  @override
+  void desligar() {
+    print('$marca $modelo está desligando...');
+  }
+
+  @override
+  String toString() {
+    return 'Smartphone -> Marca: $marca, '
+        'Modelo: $modelo, '
+        'Sistema Operacional: $sistemaOperacional';
+  }
+}
