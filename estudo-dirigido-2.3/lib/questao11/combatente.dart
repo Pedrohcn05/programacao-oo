5ca00662d8c416185c7d572769081b6699fa64d4
Pedrohcn05 <phcn.2005@gmail.com>
2026-05-18 19:41:44 -0300

    Create combatente.dart

---
+++ estudo-dirigido-2.3/lib/questao11/combatente.dart
@@ -0,0 +1,11 @@
+abstract class Combatente extends Personagem
+    implements Atacavel {
+  int forca;
+
+  Combatente(
+    String nome,
+    int vida,
+    int nivel,
+    this.forca,
+  ) : super(nome, vida, nivel);
+}
